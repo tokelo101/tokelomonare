15400bb8e9feba1e01875bc33c04e302495ad4db
tokelo101 <tokelomonare@gmail.com>
2023-11-23 12:23:56 +0200

    project setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>
+          Tokelo Monare Portfolio
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +21,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer>
+        copyright©️ 2023: tokelomonare@gmail.com
+
+      </footer>
     </div>
   );
 }
